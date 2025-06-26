886b1a8803d03e35bd826a436e94d8e914768a42
kimdaeho1 <itovel100@gmail.com>
2025-06-26 17:41:41 +0900

    image이름 변경 로직 추가

--- app/routers/image.py
+++ app/routers/image.py
@@ -3,8 +3,15 @@ from typing import List
 from app.utils.s3_upload import upload_file_to_s3
 from PIL import Image
 import io
+from datetime import datetime
 
 router = APIRouter()
+
+def generate_filename(filename: str) -> str:
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d_%H%M%S%f")[:-3]
+    extension = filename.split('.')[-1] or "jpg"
+    return f"{timestamp}.{extension}"
 
 @router.post("/v1/gik/community/images", status_code=status.HTTP_200_OK)
 async def upload_images(
@@ -17,20 +24,22 @@ async def upload_images(
     """
     # 이미지 업로드후 리턴할 이미지 URL (원본이미지 3장인 image_url_list, 섬네일 이미지 thumbnail_url)
     image_url_list = []
-    thumbnail_name = None
+    thumbnail_url = None
     try: 
         for idx, file in enumerate(images):
             s3_key=f"community/{board_id}/"
+            str_filename = generate_filename(file.filename)
+            
             file.file.seek(0)
             image_files = file.file.read()
             origin_file=io.BytesIO(image_files)
-            if not upload_file_to_s3(origin_file, s3_key, file.filename):
+            if not upload_file_to_s3(origin_file, s3_key, str_filename):
                 raise HTTPException(
                     status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                     detail=f"Failed to upload image {file.filename} to S3",
                 )
             # 원본 이미지 URL리스트에 추가
-            image_url_list.append(s3_key + file.filename)
+            image_url_list.append(s3_key + str_filename)
 
             # 첫 번재 이미지는 다운사이징 해서 섬네일로 저장하기.
             if idx == 0:
@@ -57,4 +66,38 @@ async def upload_images(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Failed to upload board images to S3 {str(e)}",
+        )
+
+
+@router.post("/v1/gik/images", status_code=status.HTTP_200_OK)
+async def upload_gik_images(
+    user_id: str,
+    image_label: str,
+    images: List[UploadFile] = File(default=None),
+):
+    """
+    사진 업로드
+    user_id: 이미지를 사용한 사용자의 id
+    image_label: 이미지 사용처 (프로필 이미지, 채팅방, 그룹채팅방..)
+    """
+
+    image_url_list = []
+    try:
+        for idx, file in enumerate(images):
+            s3_key=f"{image_label}/{user_id}/"
+            str_filename=generate_filename(file.filename)
+            if not upload_file_to_s3(file.file, s3_key, str_filename):
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Failed to upload image {file.filename} to S3",
+                )
+            
+            # 원본 이미지 URL 리스트에 추가하기
+            image_url_list.append(s3_key + str_filename)
+        return {"message": "이미지 업로드 성공", "image_urls": image_url_list}
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to upload images to S3 {str(e)}",
         )
