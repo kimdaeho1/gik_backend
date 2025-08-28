a03a33bfe15bf9867606bf4d952f922a29a70eee
kimdaeho1 <itovel100@gmail.com>
2025-08-28 16:18:43 +0900

    오타 수정

--- app/routers/token.py
+++ app/routers/token.py
@@ -20,6 +20,12 @@ async def refresh_token(
     token : 기존의 리프레시 토큰
     """
     new_tokens = create_new_tokens_based_on_refresh_token(token.credentials)
+    success: bool = await token_service.refresh_user_token(
+        new_tokens["user_id"],
+        new_tokens["access_token"],
+        new_tokens["refresh_token"]
+    )
+    
     if not success:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
